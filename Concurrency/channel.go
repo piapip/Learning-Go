6f04261aac5257691b43119471a4d1eda4d7475b
piapip <thovinh9997@gmail.com>
2019-12-05 09:46:41 +0700

    A simple example about or-channel. It's so niche though.

--- Concurrency/channel.go
+++ Concurrency/channel.go
@@ -6,6 +6,60 @@ import (
 	"time"
 )
 
+func test() {
+
+}
+
+func testOrChannel() {
+	var or func(channels ...<-chan interface{}) <-chan interface{}
+	or = func(channels ...<-chan interface{}) <-chan interface{} {
+		switch len(channels) {
+		case 0:
+			return nil
+		case 1:
+			return channels[0]
+		}
+
+		orDone := make(chan interface{})
+		go func() {
+			defer close(orDone)
+			switch len(channels) {
+			case 2:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				}
+			default:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				case <-channels[2]:
+				case <-or(append(channels[3:], orDone)...):
+				}
+			}
+		}()
+		return orDone
+	}
+
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+	)
+	fmt.Println("done after ", time.Since(start))
+}
+
 func preventGoroutineLeak() {
 
 	doWork := func(done <-chan interface{}, strings <-chan string) <-chan interface{} {
@@ -48,7 +102,7 @@ func preventGoroutineLeak() {
 	fmt.Println("Done")
 }
 
-func test() {
+func testRand() {
 	newRandStream := func(done <-chan interface{}) <-chan int {
 		randStream := make(chan int)
 		go func() {
