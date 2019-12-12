8df1e14589a9d788afc34b4602d2d0b615346cc3
piapip <thovinh9997@gmail.com>
2019-12-12 17:06:35 +0700

    Hella lot of stuff. Mostly new coding patterns

--- Concurrency/channel.go
+++ Concurrency/channel.go
@@ -7,6 +7,83 @@ import (
 	"time"
 )
 
+func test() {
+
+}
+
+func testBridge() {
+	orDone := func(done <-chan interface{}, c <-chan interface{}) <-chan interface{} {
+		valStream := make(chan interface{})
+		go func() {
+			defer close(valStream)
+			for {
+				select {
+				case <-done:
+					return
+				case v, ok := <-c:
+					if ok == false {
+						return
+					}
+					select {
+					case valStream <- v:
+					case <-done:
+					}
+				}
+			}
+		}()
+		return valStream
+	}
+
+	bridge := func(done <-chan interface{}, chanStream <-chan <-chan interface{}) <-chan interface{} {
+		valStream := make(chan interface{})
+		go func() {
+			defer close(valStream)
+			var stream <-chan interface{}
+			// stream := make(chan interface{})
+			for {
+				select {
+				case maybeStream, ok := <-chanStream:
+					if ok == false {
+						return
+					}
+					stream = maybeStream
+				case <-done:
+					return
+				}
+				for val := range orDone(done, stream) {
+					select {
+					case valStream <- val:
+					case <-done:
+					}
+				}
+			}
+		}()
+		return valStream
+	}
+
+	genVals := func() <-chan <-chan interface{} {
+		chanStream := make(chan (<-chan interface{}))
+		go func() {
+			defer close(chanStream)
+			for i := 0; i < 10; i++ {
+				stream := make(chan interface{}, 1)
+				stream <- i
+				close(stream)
+				chanStream <- stream
+			}
+		}()
+		return chanStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	for v := range bridge(done, genVals()) {
+		fmt.Printf("%v ", v)
+	}
+	fmt.Println()
+}
+
 func testTryCatch() {
 	type Result struct {
 		Error    error
--- Concurrency/fanning.go
+++ Concurrency/fanning.go
@@ -3,10 +3,106 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"runtime"
+	"sync"
 	"time"
 )
 
-func test() {
+func testFanIn() {
+	repeatFn := func(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				select {
+				case <-done:
+					return
+				case valueStream <- fn():
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	take := func(done <-chan interface{}, valueStream <-chan interface{}, num int) <-chan interface{} {
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	toInt := func(done <-chan interface{}, valueStream <-chan interface{}) <-chan int {
+		intStream := make(chan int)
+		go func() {
+			defer close(intStream)
+			for v := range valueStream {
+				select {
+				case <-done:
+					return
+				case intStream <- v.(int):
+				}
+			}
+		}()
+		return intStream
+	}
+
+	primeFinder := func(done <-chan interface{}, intStream <-chan int) <-chan interface{} {
+		primeStream := make(chan interface{})
+		go func() {
+			defer close(primeStream)
+			for integer := range intStream {
+				integer--
+				prime := true
+				for divisor := integer - 1; divisor > 1; divisor-- {
+					if integer%divisor == 0 {
+						prime = false
+						break
+					}
+				}
+
+				if prime {
+					select {
+					case <-done:
+						return
+					case primeStream <- integer:
+					}
+				}
+			}
+		}()
+		return primeStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+	start := time.Now()
+
+	rd := func() interface{} {
+		return rand.Intn(50000000)
+	}
+	randIntStream := toInt(done, repeatFn(done, rd))
+
+	numFinders := runtime.NumCPU()
+	fmt.Println("Spinning upto ", numFinders, " prime finders")
+	finders := make([]<-chan interface{}, numFinders)
+	for i := 0; i < numFinders; i++ {
+		finders[i] = primeFinder(done, randIntStream)
+	}
+	fmt.Println("Primes: ")
+	for prime := range take(done, fanIn(done, finders...), 10) {
+		fmt.Printf("%v ", prime)
+	}
+	fmt.Printf("\nSearch took: %v\n", time.Since(start))
+}
+
+func testBadExample() {
 	repeatFn := func(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
 		valueStream := make(chan interface{})
 		go func() {
@@ -53,13 +149,13 @@ func test() {
 		return intStream
 	}
 
-	//find the closest prime to the int. And why is it chan interface{}... and not chan int... a lil bit weird
+	//find the closest prime to the int. And why is it chan interface{}... and not chan int... a lil bit weird...
+	//oooh, because take channel only accepts chan interface{}
 	primeFinder := func(done <-chan interface{}, intStream <-chan int) <-chan interface{} {
 		primeStream := make(chan interface{})
 		go func() {
 			defer close(primeStream)
 			for integer := range intStream {
-				integer--
 				prime := true
 				for divisor := integer - 1; divisor > 1; divisor-- {
 					if integer%divisor == 0 {
@@ -94,4 +190,35 @@ func test() {
 		fmt.Printf("%v ", prime)
 	}
 	fmt.Printf("\nSearch took: %v\n", time.Since(start))
+}
+
+func fanIn(done <-chan interface{}, channels ...<-chan interface{}) <-chan interface{} {
+	var wg sync.WaitGroup
+	multiplexedStream := make(chan interface{})
+
+	muliplex := func(c <-chan interface{}) {
+		defer wg.Done()
+		for i := range c {
+			select {
+			case <-done:
+				return
+			case multiplexedStream <- i:
+			}
+		}
+	}
+
+	wg.Add(len(channels))
+	for _, c := range channels {
+		go muliplex(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(multiplexedStream)
+	}()
+
+	// wg.Wait()
+	// close(multiplexedStream)
+
+	return multiplexedStream
 }
