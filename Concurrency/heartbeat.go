acea011bf5de665b6d035cdb532f3c2c3b53de4e
piapip <thovinh9997@gmail.com>
2019-12-19 19:35:54 +0700

    Heartbeat...

---
+++ Concurrency/heartbeat.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func testBadHeartbeat() {
+	doWork := func(done <-chan interface{}, pulseInterval time.Duration) (<-chan interface{}, <-chan time.Time) {
+		heartbeat := make(chan interface{})
+		results := make(chan time.Time)
+		go func() {
+			// defer close(heartbeat)
+			// defer close(results)
+
+			pulse := time.Tick(pulseInterval)
+			workGen := time.Tick(2 * pulseInterval)
+
+			sendPulse := func() {
+				select {
+				case heartbeat <- struct{}{}:
+				default:
+				}
+			}
+
+			sendResult := func(r time.Time) {
+				for {
+					select {
+					case <-pulse:
+						sendPulse()
+					case results <- r:
+						return
+					}
+				}
+			}
+
+			// for{
+			for i := 0; i < 2; i++ {
+				select {
+				case <-done:
+					return
+				case <-pulse:
+					sendPulse()
+				case r := <-workGen:
+					sendResult(r)
+				}
+			}
+		}()
+		return heartbeat, results
+	}
+	done := make(chan interface{})
+	time.AfterFunc(10*time.Second, func() { close(done) })
+	const timeout = 2 * time.Second
+	heartbeat, results := doWork(done, timeout/2)
+	for {
+		select {
+		case _, ok := <-heartbeat:
+			if ok == false {
+				return
+			}
+			fmt.Println("Pulse")
+		case r, ok := <-results:
+			if ok == false {
+				return
+			}
+			fmt.Println("Results ", r.Second())
+		case <-time.After(timeout):
+			fmt.Println("worker goroutine is not healthy!")
+			return
+		}
+	}
+}
+
+func test() {
+	doWork := func(done <-chan interface{}, pulseInterval time.Duration) (<-chan interface{}, <-chan time.Time) {
+		heartbeat := make(chan interface{})
+		results := make(chan time.Time)
+		go func() {
+			defer close(heartbeat)
+			defer close(results)
+
+			pulse := time.Tick(pulseInterval)
+			workGen := time.Tick(2 * pulseInterval)
+
+			sendPulse := func() {
+				select {
+				case heartbeat <- struct{}{}:
+				default:
+				}
+			}
+
+			sendResult := func(r time.Time) {
+				for {
+					select {
+					case <-done:
+						return
+					case <-pulse:
+						sendPulse()
+					case results <- r:
+						return
+					}
+				}
+			}
+
+			for {
+				select {
+				case <-done:
+					return
+				case <-pulse:
+					sendPulse()
+				case r := <-workGen:
+					sendResult(r)
+				}
+			}
+		}()
+		return heartbeat, results
+	}
+	done := make(chan interface{})
+	time.AfterFunc(11*time.Second, func() {
+		close(done)
+	})
+	const timeout = 2 * time.Second
+	heartbeat, results := doWork(done, timeout/2)
+	for {
+		select {
+		case _, ok := <-heartbeat:
+			if ok == false {
+				return
+			}
+			fmt.Println("Pulse")
+		case r, ok := <-results:
+			if ok == false {
+				return
+			}
+			fmt.Println("Results ", r.Second())
+		case <-time.After(timeout):
+			fmt.Println("If this shows up then there's a goroutine problem")
+			return
+		}
+	}
+}
